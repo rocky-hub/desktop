f8ba1cec654cd40ecb86644cec09fe95d46affbc
huangdongcheng <934313141@qq.com>
2020-01-05 22:24:52 +0800

    draw plain text

--- plaintexteditor.h
+++ plaintexteditor.h
@@ -2,14 +2,35 @@
 #define PLAINEDITOR_H
 
 #include <QPlainTextEdit>
+#include <QPaintEvent>
+#include <QResizeEvent>
+#include <QSize>
 #include <QWidget>
 
 class PlainTextEditor: public QPlainTextEdit
 {
 public:
     PlainTextEditor(QWidget *parent = nullptr);
-    ~PlainTextEditor();
-    quint32 lineNumberAreaWidth();
+
+    int lineNumberAreaWidth() const;
+    void lineNumberAreaPaintEvent(QPaintEvent *event);
+
+    QSize sizeHint() const override
+    {
+        return QSize(lineNumberAreaWidth(), 0);
+    }
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+    void paintEvent(QPaintEvent *event) override {
+        lineNumberAreaPaintEvent(event);
+    }
+
+private slots:
+    void updateLineNumberAreaWidth(int newBlockCount);
+    void updateLineNumberArea(const QRect&, int);
+    void highlightCurrentLine();
 };
 
 #endif // PLAINEDITOR_H
